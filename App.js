02cbd555f5b35d1f1fea83f82d50ed068148808c
Andrzej Legutko <andrzejlegutko@gmail.com>
2023-02-18 22:54:06 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useEffect, useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [num, setNum] = useState("#333");
+
+    return (
+        <View style={styles.container}>
+            <Text style={{color: '#fff'}}>{tekst}</Text>
+
+            <TextInput
+                placeholder={'Siema'}
+                onChangeText={(s) => setNum(s)}
+            />
+
+            <Button title={'Test'}/>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: num,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
